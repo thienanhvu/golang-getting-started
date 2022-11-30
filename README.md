7816cace9cb57a0d86ceaf73e237bfa887f5ced5
rohinivsenthil <rohinivsenthil@gmail.com>
2022-11-30 12:40:26 +0530

    feat: add logging

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Getting Started with Golang
 
-**Module Number**: 1  
-**Branch**: simple-web-server
+**Module Number**: 2  
+**Branch**: logging
 
 ### This branch covers:
 
-- Creating a basic server with Golang
-- Reading and Writing to a file
-- GET and POST routes
+- Add [Logrus](https://github.com/sirupsen/logrus) for logging
+- Add logging for INFO and ERROR
+
+**Previous Module**: [Simple Web Server](https://github.com/rohinivsenthil/golang-getting-started/tree/simple-web-server)
--- main.go
+++ main.go
@@ -6,16 +6,21 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case "GET":
+			log.Info("Received get requests")
+
 			// read from file
 			data, err := ioutil.ReadFile("data.json")
 			if err != nil {
 				// return error in response
+				log.WithError(err).Error("Failed to read from data.json")
 				fmt.Fprintf(w, "Failed to get data: %s", err.Error())
 				return
 			}
@@ -23,9 +28,12 @@ func main() {
 			// set response
 			w.Write(data)
 		case "POST":
+			log.Info("Received post requests")
+
 			// read POST request body
 			data, err := io.ReadAll(r.Body)
 			if err != nil {
+				log.WithError(err).Error("Failed to read data from post")
 				fmt.Fprintf(w, "Failed to get data: %s", err.Error())
 				return
 			}
@@ -33,12 +41,14 @@ func main() {
 			// read existing data from file
 			existing_data, err := ioutil.ReadFile("data.json")
 			if err != nil {
+				log.WithError(err).Error("Failed to read from data.json")
 				fmt.Fprintf(w, "Failed to get existing data: %s", err.Error())
 			}
 
 			// parse existing string data to json
 			var todos []string
 			if err = json.Unmarshal(existing_data, &todos); err != nil {
+				log.WithError(err).Error("Failed to parse existing data")
 				fmt.Fprintf(w, "Failed to parse existing data: %s", err.Error())
 				return
 			}
@@ -48,11 +58,13 @@ func main() {
 			// convert parsed data to string
 			output, err := json.Marshal(todos)
 			if err != nil {
+				log.WithError(err).WithField("todos", todos).Error("Failed to convert todos to json")
 				fmt.Fprintf(w, "Failed to convert to json: %s", err.Error())
 			}
 
 			// write to file
 			if err = ioutil.WriteFile("data.json", output, 0666); err != nil {
+				log.WithError(err).WithField("data", output).Error("Failed to write data to data.json")
 				fmt.Fprintf(w, "Failed to write to data.json: %s", err.Error())
 			}
 
